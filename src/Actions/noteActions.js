a7adc221ca9dd55d58e155c5ea6cde408cc3641f
Richie <richphinsalah6@gmail.com>
2021-07-08 19:25:28 +0000

    CRUD(U and D with firestore using async)

--- src/Actions/noteActions.js
+++ src/Actions/noteActions.js
@@ -3,59 +3,58 @@ import React from 'react'
 export  function getAllNotes() {
     return(dispatch,state,{getFirestore})=>{
        const db = getFirestore();
-       db.collection('notes')
-       .get()
-       .then((results)=>{
-           let notes =[];
-           results.forEach((doc)=>{
-               notes.push(doc.data());
-           });
-           dispatch({
-               type: 'ADD_ALL_NOTES',
-               payload: notes,
-             });
-       })
-       .catch((err) => {
-           console.log(err);
-         });
+       db.collection('notes').onSnapshot(
+        (results)=>{
+            let notes =[];
+            results.forEach((doc)=>{
+                notes.push({...doc.data(), id: doc.id});
+            });
+            dispatch({
+                type: 'ADD_ALL_NOTES',
+                payload: notes,
+              });
+        },
+        (err) => {
+            console.log(err);
+          }
+       );
+       
     };
    
 }
 
 export function addNotes(newNotes) {
-    return(dispatch,state,{getFirestore})=>{
+    return async (dispatch,state,{getFirestore})=>{
         const db = getFirestore();
-        db.collection('notes')
-        .add(newNotes)
-        .then(()=>{
-            dispatch(
-                {
-                    type: "ADD_NOTES",
-                    payload: newNotes,
-                }
-            );
-        })
-        .catch((err) => {
-            console.log(err);
-          });
-
-    }
+        try {
+           await db.collection('notes').add(newNotes)  
+        } catch (error) {
+            console.log(error);
+        }
+    }  
 }
 
-export  function deleteNote(id) {
-     
-    return{
-       type:"DELETE_NOTE",
-       payload: id
-    }
-}
 
-export  function updateNote(id,updatednote) {
-    console.log("nau",updatednote)
-     
-    return{
-       type:"UPDATE_NOTE",
-       payload:{ id:id , updatednoteinfo:updatednote},
-       
+ export  function deleteNote(id) {
+    return async (dispatch,state,{getFirestore})=>{
+        const db = getFirestore();
+        try {
+           await db.collection('notes').doc(id).delete();
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+
+ }
+
+ export  function updateNote(id,updatednote) {
+    return async (dispatch,state,{getFirestore})=>{
+        const db = getFirestore();
+        try {
+           await db.collection('notes').doc(id).update(updatednote);
+        } catch (error) {
+            console.log(error);
+        }
     }
+
 }
